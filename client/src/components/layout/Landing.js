44c57ddd849e19b44f6b5d292730a47d8753d58e
erwanriou <rr.1.riou@gmail.com>
2018-10-17 00:35:55 +0200

    [Fix] - Update translation bug

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -16,24 +16,24 @@ class Landing extends React.Component {
             <div className="title">
               <img src={icon} alt="logo bebeyogini"/>
               <h2>
-                <Translate id="banner.quote" />
+                <Translate id="landing.quote" />
               </h2>
             </div>
             <div className="links">
               <Link to='/feed'>
-              <h3>Blog</h3>
+                <h3><Translate id="landing.blog" /></h3>
                 <img src={blog} alt="icon of the blog"/>
-                <p><Translate id="banner.link" /></p>
+                <p><Translate id="landing.link" /></p>
               </Link>
               <span>
-                <h3>School</h3>
+                <h3><Translate id="landing.school" /></h3>
                 <img src={school} alt="icon of the blog"/>
-                <p><Translate id="banner.soon" /></p>
+                <p><Translate id="landing.soon" /></p>
               </span>
               <span>
-                <h3>Shop</h3>
+                <h3><Translate id="landing.shop" /></h3>
                 <img src={shop} alt="icon of the blog"/>
-                <p><Translate id="banner.soon" /></p>
+                <p><Translate id="landing.soon" /></p>
               </span>
             </div>
           </div>
