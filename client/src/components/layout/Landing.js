7e89bc8e5b0d39de4051ec836266a6bdeb05f12e
erwanriou <rr.1.riou@gmail.com>
2018-10-17 00:28:43 +0200

    [Project] - Remove banner and implement the landing without without it

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,11 +1,43 @@
 import React from 'react'
-import Banner from './Banner'
+import { Link } from 'react-router-dom'
+import { Translate} from 'react-localize-redux'
+
+import icon from '../../utils/images/logobbyogini2.png'
+import blog from '../../utils/images/blog.png'
+import school from '../../utils/images/school.png'
+import shop from '../../utils/images/shop.png'
 
 class Landing extends React.Component {
   render() {
     return (
       <div className="landing">
-        <Banner />
+        <div className="opacity">
+          <div className="container">
+            <div className="title">
+              <img src={icon} alt="logo bebeyogini"/>
+              <h2>
+                <Translate id="banner.quote" />
+              </h2>
+            </div>
+            <div className="links">
+              <Link to='/feed'>
+              <h3>Blog</h3>
+                <img src={blog} alt="icon of the blog"/>
+                <p><Translate id="banner.link" /></p>
+              </Link>
+              <span>
+                <h3>School</h3>
+                <img src={school} alt="icon of the blog"/>
+                <p><Translate id="banner.soon" /></p>
+              </span>
+              <span>
+                <h3>Shop</h3>
+                <img src={shop} alt="icon of the blog"/>
+                <p><Translate id="banner.soon" /></p>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
