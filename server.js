9287ad1e2b270954bb9ce638abab0684a105a871
erwanriou <rr.1.riou@gmail.com>
2020-03-07 19:08:50 +0100

    [Project] - Architecture changes 2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const compression = require("compression")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const passport = require("passport")
@@ -36,11 +37,11 @@ app.use(helmet())
 app.disable("x-powered-by")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(compression())
 app.use(passport.initialize())
 
 // Connect to Mongodb
 const mongoDb = mlab()
-mongoose.set("useCreateIndex", true)
 
 // Use Routes
 app.use("/auth/google", google)
@@ -53,7 +54,7 @@ app.use("/api/posts", posts)
 if (process.env.NODE_ENV === "production") {
   app.enable("trust proxy")
   app.use(wwwRedirect)
-  app.use(express.static("client/build"))
+  app.use(express.static("client/build", { maxAge: 31536000 }))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
--- services/mlab.js
+++ services/mlab.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 // DB config
-const db = require("../config/keys").keys;
+const db = require("../config/keys").keys
 
 module.exports = async () => {
   try {
-    await mongoose.set("useNewUrlParser", true);
-    await mongoose.set("useFindAndModify", false);
-    await mongoose.set("useCreateIndex", true);
-    await mongoose.set("useUnifiedTopology", true);
-    await mongoose.connect(db.mongo.url(), db.mongo.options);
+    await mongoose.set("useNewUrlParser", true)
+    await mongoose.set("useFindAndModify", false)
+    await mongoose.set("useCreateIndex", true)
+    await mongoose.set("useUnifiedTopology", true)
+    await mongoose.connect(db.mongo.url(), db.mongo.options)
 
-    console.log("Mongodb Connected");
+    console.log("Mongodb Connected")
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
